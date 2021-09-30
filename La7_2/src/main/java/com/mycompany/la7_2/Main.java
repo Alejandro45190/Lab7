c854c389810e49683f18f5209290d86175199d19
Alejandro Rivas <alejandro.rivas@go.tarleton.edu>
2021-09-30 17:26:45 -0500

    deleted unnecessary code

--- La7_2/src/main/java/com/mycompany/la7_2/Main.java
+++ La7_2/src/main/java/com/mycompany/la7_2/Main.java
@@ -1,11 +1,8 @@
 package com.mycompany.la7_2;
 
-import edu.tarleton.caesarcipher.Caesar;
-
 public class Main {
     
     public static void main(String[] args) {
-        Caesar cipher = new Caesar(2);
         Encrypt enc = new Encrypt();
         enc.encrypt();
         Decrypt dec = new Decrypt();
